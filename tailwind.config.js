3d55e71ee0525b347ed8d7dd090dcdcc7be52aec
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2024-12-01 18:16:09 +0600

    add google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        body: ["Inter", "sans-serif"],
+        heading: ["Playfair Display", "serif"],
+      },
     },
   },
   plugins: [],
